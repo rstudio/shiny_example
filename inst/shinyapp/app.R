7923c05f95e0ae03631635d39df63246114eec2e
Winston Chang <winston@stdout.org>
2022-06-17 21:27:05 -0500

    Update code in inst/

---
+++ inst/shinyapp/app.R
@@ -0,0 +1,57 @@
+library(shiny)
+
+ui <- fluidPage(
+  selectInput(
+    "n_breaks",
+    label = "Number of bins in histogram (approximate):",
+    choices = c(10, 20, 35, 50),
+    selected = 20
+  ),
+
+  checkboxInput(
+    "individual_obs",
+    label = strong("Show individual observations"),
+    value = FALSE
+  ),
+
+  checkboxInput(
+    "density",
+    label = strong("Show density estimate"),
+    value = FALSE
+  ),
+
+  plotOutput("main_plot", height = "300px"),
+
+  # Display this only if the density is shown
+  conditionalPanel(condition = "input.density == true",
+    sliderInput(
+      "bw_adjust",
+      label = "Bandwidth adjustment:",
+      min = 0.2, max = 2, value = 1, step = 0.2)
+  )
+)
+
+server <- function(input, output) {
+
+  output$main_plot <- renderPlot({
+    hist(
+      faithful$eruptions,
+      probability = TRUE,
+      breaks = as.numeric(input$n_breaks),
+      xlab = "Duration (minutes)",
+      main = "Geyser eruption duration"
+    )
+
+    if (input$individual_obs) {
+      rug(faithful$eruptions)
+    }
+
+    if (input$density) {
+      dens <- density(faithful$eruptions, adjust = input$bw_adjust)
+      lines(dens, col = "blue")
+    }
+  })
+}
+
+
+shinyApp(ui, server)
